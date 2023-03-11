0da39fd6c8efbc24253c2fc31bc09f8807cfc2d3
Lana Temkaieva <havchikos@gmail.com>
2023-03-11 10:33:14 +0100

    Added switching between the turn of players 1 and 2

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -9,10 +9,11 @@ import javafx.scene.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class GameController {
-	
-	
 	@FXML private Pane pane0;
 	@FXML private Text sign0;
 	@FXML private Pane pane1;
@@ -31,14 +32,25 @@ public class GameController {
 	@FXML private Text sign7;
 	@FXML private Pane pane8;
 	@FXML private Text sign8;
+	@FXML private ImageView signImg0;
+	
+	@FXML 
+	private Text playersSwitch;
 	
 	private ArrayList<Integer> game = new ArrayList<Integer>();
 	private static String x = "⚔️";
 	private static String o = "O️";
+	
+	private Boolean player1 = true;
 
 	
 	@FXML public void handlePlay(MouseEvent event) {
 		System.out.println("Get pane clicked");
+//		Image imageO = new Image("./resources/images/TicTacToe/circle.png");
+//		Image imageX = new Image("./resources/images/TicTacToe/cross.png");
+		
+//		signImg0.setImage(imageO);
+		
 		Text textPlayed;
 		if(event.getTarget() instanceof Pane) {
 			textPlayed = (Text)((Pane) event.getTarget()).getChildren().get(0);
@@ -48,7 +60,16 @@ public class GameController {
 		
 		int posPlayed = Character.getNumericValue( textPlayed.getId().charAt(textPlayed.getId().length() - 1));
 		
-		textPlayed.setText(x);
+		if (player1) {
+			textPlayed.setText(x);
+			playersSwitch.setText("Player : 2");
+			player1 = false;
+		} else {
+			textPlayed.setText(o);
+			playersSwitch.setText("Player : 1");
+			player1 = true;
+		}
+
 		
 		System.out.println("Get the id");
 		System.out.println("You clicked ON " + posPlayed);
