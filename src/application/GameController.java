e8fdfcc9d2c0021621ac2edb7fe7b97beb89a845
Lana Temkaieva <havchikos@gmail.com>
2023-04-09 10:53:42 +0200

    Added new characters to the grid. The 'new game' button works

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -18,7 +18,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import javafx.scene.control.Button;
-
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
@@ -30,26 +30,28 @@ public class GameController {
 	private static final int ROWS = 3;
     private static final int COLUMNS = 3;
     
+    private static String XSign = "file:src/style/x.png";
+	private static String OSign = "file:src/style/Opiece.png";
+    
 	
 	@FXML private Pane pane0;
-	@FXML private Text sign0;
+	@FXML private ImageView sign0;
 	@FXML private Pane pane1;
-	@FXML private Text sign1;
+	@FXML private ImageView sign1;
 	@FXML private Pane pane2;
-	@FXML private Text sign2;
+	@FXML private ImageView sign2;
 	@FXML private Pane pane3;
-	@FXML private Text sign3;
+	@FXML private ImageView sign3;
 	@FXML private Pane pane4;
-	@FXML private Text sign4;
+	@FXML private ImageView sign4;
 	@FXML private Pane pane5;
-	@FXML private Text sign5;
+	@FXML private ImageView sign5;
 	@FXML private Pane pane6;
-	@FXML private Text sign6;
+	@FXML private ImageView sign6;
 	@FXML private Pane pane7;
-	@FXML private Text sign7;
+	@FXML private ImageView sign7;
 	@FXML private Pane pane8;
-	@FXML private Text sign8;
-	@FXML private ImageView signImg0;
+	@FXML private ImageView sign8;
 	
 	@FXML 
 	private Text playersSwitch;
@@ -57,18 +59,13 @@ public class GameController {
 	List<FillTransition> transitionList = new ArrayList<>();
 	List<ScaleTransition> scaleTransitionList = new ArrayList<>();
 	
+	String strWinner;
+	
 	@FXML
 	GridPane gridPane;
 	
 	@FXML 
 	private Button btnNewGame;
-	
-	private ArrayList<String> game = new ArrayList<>();
-	{
-		for (int i = 0; i < ROWS * COLUMNS; i++) {
-	        game.add("");
-	    }
-	}
 	
 	private Game gameLogic = new Game();
 	
@@ -86,14 +83,18 @@ public class GameController {
 	@FXML 
 	public void handleNewGame(ActionEvent event) {
 		gridPane.setDisable(false);
+		
 	    // Очистить все поля Text внутри Pane
-	    for (Node node : pane0.getParent().getChildrenUnmodifiable()) {
-	        if (node instanceof Pane) {
-	            ((Text) ((Pane) node).getChildren().get(0)).setText("");
-	            ((Text) ((Pane) node).getChildren().get(0)).setDisable(false);
-	            node.setDisable(false);
-	        }
-	    }
+		for (Node node : pane0.getParent().getChildrenUnmodifiable()) {
+		    if (node instanceof Pane) {
+		        for (Node childNode : ((Pane) node).getChildren()) {
+		            if (childNode instanceof ImageView) {
+		                ((ImageView) childNode).setImage(null);
+		            }
+		        }
+		        node.setDisable(false);
+		    }
+		}
 	    
 	    for (FillTransition transition : transitionList) {
 	        transition.stop();
@@ -108,18 +109,25 @@ public class GameController {
 	    scaleTransitionList.clear();
 	    
 	    // Сбросить игру
-	    game.clear();
-	    for (int i = 0; i < ROWS * COLUMNS; i++) {
-	        game.add("");
-	    }
 
 	    playerX = true;
 	    playersSwitch.setText("Player : X");
 	    btnNewGame.setVisible(false);
 	    gameLogic =  new Game();
-	    
 
 	}
+	
+	
+	@FXML 
+	public void testClick() {
+		System.out.println("testClickIMG");
+	}
+	
+	@FXML 
+	public void testClick2() {
+		System.out.println("testClickPane");
+	}
+	
 	/**
 	 * The method is triggered when a user clicks on a cell in the game grid. 
 	 * It updates the game state, checks for a winner, and adds animations for winning symbols.
@@ -129,39 +137,45 @@ public class GameController {
 	@FXML 
 	public void handlePlay(MouseEvent event) {
 		System.out.println("Get pane clicked");
-		
 		Pane clickedPane = (Pane) event.getSource();
-		Text textPlayed = (Text) clickedPane.getChildren().get(0);
 	    int posPlayed = Integer.parseInt(clickedPane.getId().substring(4));
+	    ImageView imgPlayed = (ImageView) clickedPane.getChildren().get(0);
 	    clickedPane.setDisable(true);
 		
 		if (playerX) {
-	        textPlayed.setText(X);
-	        game.set(posPlayed, X);
+			imgPlayed.setImage(new Image(XSign));
 	        playersSwitch.setText("Player : O");
 	        gameLogic.play(posPlayed, -1);
 	    } else {
-	        textPlayed.setText(O);
-	        game.set(posPlayed, O);
+	    	imgPlayed.setImage(new Image(OSign));
 	        playersSwitch.setText("Player : X");
 	        gameLogic.play(posPlayed, 1);
 	    }
 		
 		playerX = !playerX;
 
-		String winner = checkWin();
+		double winner = gameLogic.checkIfWin();
 		
-		if (winner != null) {
-		    if (winner.equals(X) || winner.equals(O)) {
-		    	System.out.println(winner + " wins!");
-		        playersSwitch.setText(winner + " wins!");
+		if (winner != 0) {
+			if (winner == 1.0)
+				strWinner = O;
+			else if (winner == -1.0) {
+				strWinner = X;
+			} else {
+				strWinner = "Tie";
+			}
+			
+			if (strWinner.equals(X) || strWinner.equals(O)) {
+		    	System.out.println(strWinner + " wins!");
+		        playersSwitch.setText(strWinner + " wins!");
 		        btnNewGame.setVisible(true);
 		        gridPane.setDisable(true);
-		    } else if (winner.equals("Tie")) {
+		    } else if (strWinner.equals("Tie")) {
 		        playersSwitch.setText("It's a tie!");
 		        btnNewGame.setVisible(true);
 		        gridPane.setDisable(true);
 		    }
+			
 		    System.out.println("GameLogique");
 		    System.out.println(gameLogic);
 		    int[] posWin = gameLogic.getWinPos();
@@ -171,17 +185,10 @@ public class GameController {
 		        Color endColor = Color.GREEN;
 		        int cycleCount = FillTransition.INDEFINITE;
 		        
-			    for(int e : posWin) {
+		        for(int e : posWin) {
 			    	System.out.println("pos : " + e);
-			    	Text winner_field = (Text) ((Pane) gridPane.getChildren().get(e)).getChildren().get(0);
-			    	//winner_field.
+			    	ImageView winner_field = (ImageView) ((Pane) gridPane.getChildren().get(e)).getChildren().get(0);
 
-			        FillTransition fillTransition = new FillTransition(duration, winner_field, startColor, endColor);
-			        fillTransition.setCycleCount(cycleCount);
-			        fillTransition.setAutoReverse(true);
-			        fillTransition.play();
-			        transitionList.add(fillTransition);
-			        
 			        
 			        ScaleTransition scaleTransition = new ScaleTransition(duration, winner_field);
 			        scaleTransition.setFromX(1);
@@ -203,49 +210,5 @@ public class GameController {
 	 * If a player has won, the method returns the player's symbol.
 	 * If there is no winner, the method returns null.
 	 */
-	private String checkWin() {
-		 // Check verticals
-        for (int col = 0; col < COLUMNS; col++) {
-            if (!game.get(col).isEmpty() &&
-                game.get(col).equals(game.get(col + ROWS)) &&
-                game.get(col).equals(game.get(col + ROWS * 2))) {
-                return game.get(col);
-            }
-        }
-	    // Проверяем горизонтали
-		for (int row = 0; row < ROWS; row++) {
-			int start = row * COLUMNS;
-			if (game.get(start).equals(game.get(start + 1)) && game.get(start).equals(game.get(start + 2)) && !game.get(start).isEmpty()) {
-			return game.get(start);
-	        }
-	    }
-
-	    // Проверяем вертикали
-	    for (int col = 0; col < COLUMNS; col++) {
-	        if (game.get(col).equals(game.get(col + COLUMNS)) && game.get(col).equals(game.get(col + COLUMNS * 2))) {
-	        	System.out.println("winner 2" + col);
-	            return game.get(col);
-	        }
-	    }
-
-	    // Проверяем диагонали
-	    if (game.get(0).equals(game.get(4)) && game.get(0).equals(game.get(8))) {
-	    	System.out.println("winner 3" + game.get(0));
-	        return game.get(0);
-	    }
-	    
-	    if (game.get(2).equals(game.get(4)) && game.get(2).equals(game.get(6))) {
-	    	System.out.println("winner 4" + game.get(2));
-	        return game.get(2);
-	    }
-	    
-
-	    if (game.stream().anyMatch(String::isEmpty)) {
-	        return null;
-	    } else {
-	        return "Tie";
-	    }
-
-	}
-
+	
 }
